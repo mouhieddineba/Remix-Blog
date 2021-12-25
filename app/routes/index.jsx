3cd49c28facff6bccfbd21ea2fc3fd7399c56816
Mouhieddine El-Bathish <mouhieddine.bathish1212@gmail.com>
2021-12-25 17:42:09 +0200

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My blog App!</h1>
+      <p>
+        This is a full stack blog app Built using the Remix framework and prisma
+        database.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
